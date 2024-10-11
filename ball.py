def2d76247ec8aaf14e5b4ce248cb8031b40a5d8
Korab <korab.morina44@gmail.com>
2024-10-11 13:52:02 +0300

    finale

--- ball.py
+++ ball.py
@@ -10,13 +10,20 @@ class Ball(Turtle):
         self.goto(0, 0)
         self.y_move = 10
         self.x_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         xcor = self.xcor() + self.x_move
         yxor = self.ycor() + self.y_move
         self.goto(xcor, yxor)
+    def reset(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
     def bounce_y(self):
         self.y_move *= -1
+        self.move_speed *= 0.9
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 import time
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Pong Game")
@@ -11,7 +12,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard =Scoreboard()
 
 
 screen.listen()
@@ -22,7 +23,7 @@ screen.onkey(l_paddle.move_down, "s")
 
 game_on = True
 while game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     if ball.ycor() > 250 or ball.ycor() < -250:
@@ -30,6 +31,12 @@ while game_on:
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320 :
         ball.bounce_x()
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset()
+        scoreboard.r_point()
 
 
 
